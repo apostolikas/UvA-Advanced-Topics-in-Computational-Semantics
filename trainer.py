6ed8c73d270ae4a9441207d9b737c5568ba11b8e
Nick Apostolikas <n.apostolikas@hotmail.com>
2023-04-08 13:36:45 +0200

    trainer updated

--- trainer.py
+++ trainer.py
@@ -2,8 +2,9 @@ import torch
 import torchtext
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
 from utils import train_data, valid_data, test_data
-from models import Enc_MLP
+from models import *
 import os
 import argparse
 
@@ -12,19 +13,26 @@ def train(args):
 
     os.makedirs(args.log_dir, exist_ok=True)
 
+    logger = TensorBoardLogger('model_logs', args.encoder_type)
+    checkpoint_callback = ModelCheckpoint(monitor='val_acc', dirpath=f"checkpoints/{args.encoder_type}", filename=f"version_{logger.version}")
+    lr_monitor = LearningRateMonitor('epoch')
+
     train_dataloader = torchtext.data.BucketIterator(train_data, batch_size=args.batch_size, train=True)
     valid_dataloader = torchtext.data.BucketIterator(valid_data, batch_size=args.batch_size, train=True)
     test_dataloader = torchtext.data.BucketIterator(test_data, batch_size=args.batch_size, train=False)
 
     trainer = pl.Trainer(default_root_dir=args.log_dir,
-                         gpus=1 if torch.cuda.is_available() else 0,
-                         max_epochs = 30,
-                         checkpoint_callback=ModelCheckpoint(save_weights_only=True, monitor="val_acc", mode="max"),
+                         accelerator = 'gpu' if torch.cuda.is_available() else 'cpu',
+                         max_epochs = 1,
+                         logger=[logger],
+                         callbacks=[lr_monitor, checkpoint_callback],
                          enable_progress_bar=True)
+    
+    
     trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
 
     pl.seed_everything(args.seed)  # To be reproducible
-    model = Enc_MLP(encoder_type=args.encoder_type)
+    model = Enc_MLP(args)
 
     # Training
     trainer.fit(model,train_dataloader,valid_dataloader)
@@ -32,13 +40,13 @@ def train(args):
     # Testing
     model = Enc_MLP.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
     test_result = trainer.test(model, dataloaders=test_dataloader, verbose=True)
-
     return test_result
+
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArguementParser(
+    parser = argparse.ArgumentParser(
         formatter_class = argparse.ArgumentDefaultsHelpFormatter)
     
     # Model hyperparameters
@@ -46,7 +54,7 @@ if __name__ == "__main__":
                         help='Type of encoder (awe,uni_lstm,bi_lstm,pooled_bi_lstm)')
 
     # Optimizer hyperparameters
-    parser.add_argument('--lr', default=0.02, type=float,
+    parser.add_argument('--lr', default=0.1, type=float,
                         help='Learning rate')
     parser.add_argument('--batch_size', default=64, type=int,
                         help='Batch size')
@@ -64,4 +72,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    train(args)
+    test_res = train(args)
