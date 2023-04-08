e7d3057625ac2b270af893c0df5158da0dd32885
Nick Apostolikas <n.apostolikas@hotmail.com>
2023-04-08 18:34:52 +0200

    utils updated

--- utils.py
+++ utils.py
@@ -3,25 +3,26 @@ import spacy
 import os
 import pickle
 
-# spacy is needed in order to tokenize the corpus, so we download it
 if not spacy.util.is_package("en_core_web_sm"):
     import spacy.cli
     spacy.cli.download('en_core_web_sm')
 
-# we build the tokenized corpus
-TEXT = data.Field(tokenize='spacy', tokenizer_language='en_core_web_sm', batch_first=True)
-LABEL = data.Field(sequential=False, is_target=True, unk_token=None)
+TEXT = data.Field(sequential=True, tokenize='spacy', tokenizer_language='en_core_web_sm', batch_first=True, lower=True, use_vocab=True, include_lengths=False ,fix_length=300)
+LABEL = data.Field(sequential=False, use_vocab=True, pad_token=None, unk_token=None, batch_first=True)
 
-train_data, valid_data, test_data = datasets.nli.SNLI.splits(TEXT, LABEL, root = './data')
+train_data, valid_data, test_data = datasets.nli.SNLI.splits(TEXT, LABEL, root = 'data')
 
 # we build the vocab if does not exit
-if not os.path.exists('./data/vocab.pkl'):
-    TEXT.build_vocab(train_data, vectors = vocab.GloVe(name='840B', dim=300))
-    with open("vocab.pkl", 'wb') as f:
-        pickle.dump(TEXT.vocab, f)
-else:
+if os.path.exists("vocab.pkl"):
+    print("Loading vocabulary...")
     with open("vocab.pkl", 'rb') as f:
         TEXT.vocab = pickle.load(f)
+else:
+    print("Building vocabulary...")
+    TEXT.build_vocab(train_data, vectors=vocab.GloVe(name='840B', dim=300))
+    with open("vocab.pkl", 'wb') as f:
+        pickle.dump(TEXT.vocab, f)
 LABEL.build_vocab(train_data)
+VOCABULARY = TEXT.vocab
 
 print('Data loaded successfully!')
